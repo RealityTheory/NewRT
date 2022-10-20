5979124a14f233673a2cb4dae368b66e9c3676d0
Digital-Luminous <109281747+Digital-Luminous@users.noreply.github.com>
2022-10-20 01:07:02 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,6 @@ var createScene = function () {
 	
 	// This stops the camera from zooming at all on desktop
 	camera.inputs.removeByType("ArcRotateCameraMouseWheelInput");
-	//camera.inputs.removeByType("multiTouchPanAndZoom");
 	// and for mobile
 	camera.inputs.attached.pointers.multiTouchPanAndZoom = false;
 	camera.inputs.attached.pointers.multiTouchPanning = false;
